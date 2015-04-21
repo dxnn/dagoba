135294939ffd0b81b19596f9593f6169fa002902
dann toliver <dann@bentobox.net>
2015-04-21 02:35:23 -0400

    updating 6to5 to babel

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,11 @@
 /*
- * - run 6to5
+ * - run babel
  * - build sourcemaps
  */
 
 var gulp = require('gulp')
 
-var to5 = require('gulp-6to5')
+var babel = require('gulp-babel')
 var sourcemaps = require('gulp-sourcemaps')
 
 var buildList = [ 'dagoba.js' ]
@@ -13,7 +13,7 @@ var buildList = [ 'dagoba.js' ]
 gulp.task('build', function () {
   gulp.src(buildList)
     .pipe(sourcemaps.init())
-    .pipe(to5())
+    .pipe(babel())
     .pipe(sourcemaps.write('.'))
     .pipe(gulp.dest('build'))
 })
