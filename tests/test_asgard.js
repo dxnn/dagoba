ad65c0bd9cba9b5f5f604256c3e8ed87e5cbdbd8
dann toliver <dann@bentobox.net>
2015-04-23 00:38:59 -0400

    genderization complete

--- tests/test_asgard.js
+++ tests/test_asgard.js
@@ -11,17 +11,24 @@ describe('Asgard', function() {
     })
 
     it('should add the Aesir', function() {
-      var aesir = [ 'Ymir', 'Þrúðgelmir', 'Bergelmir', 'Búri', 'Borr', 'Bölþorn', 'Bestla', 'Odin', 'Vili', 'Ve'
-                  , 'Hœnir', 'Ægir', 'Rán', 'Fjörgynn', 'Frigg', 'Heimdallr', 'Nörfi', 'Jörð', 'Nepr', 'Gríðr'
-                  , 'Rindr', 'Dellingr', 'Nótt', 'Nanna', 'Baldr', 'Höðr', 'Hermóðr', 'Bragi', 'Iðunn', 'Víðarr'
-                  , 'Váli', 'Skjöldr', 'Gefjon', 'Ullr', 'Sif', 'Nine sisters', 'Thor', 'Járnsaxa', 'Týr', 'Dagr'
-                  , 'Forseti', 'Scyldings', 'Móði', 'Þrúðr', 'Magni'
+      var aesir = [ ['Ymir', 'M'], ['Þrúðgelmir', 'M'], ['Bergelmir', 'M'], ['Búri', 'M'], ['Borr', 'M']
+                  , ['Bölþorn', 'M'], ['Bestla', 'F'], ['Odin', 'M'], ['Vili', 'M'], ['Ve', 'M']
+                  , ['Hœnir', 'M'], ['Fjörgynn', 'M'], ['Frigg', 'F']
+                  , ['Jörð', 'F'], ['Nepr', 'M'], ['Gríðr', 'F'], ['Forseti', 'M']
+                  , ['Rindr', 'F'], ['Dellingr', 'M'], ['Nótt', 'F'], ['Nanna', 'F'], ['Baldr', 'M']
+                  , ['Höðr', 'M'], ['Hermóðr', 'M'], ['Bragi', 'M'], ['Iðunn', 'F'], ['Víðarr', 'M']
+                  , ['Váli', 'M'], ['Gefjon', 'F'], ['Ullr', 'M'], ['Týr', 'M'], ['Dagr', 'M']
+                  , ['Thor', 'M'], ['Sif', 'F'], ['Járnsaxa', 'F']
+                  , ['Móði', 'M'], ['Þrúðr', 'F'], ['Magni', 'M']
+                  , ['Ægir', 'M'], ['Rán', 'F'], ['Nine sisters', 'F'], ['Heimdallr', 'M']
                   ]
 
-      aesir.forEach(function(name) { g.addVertex({_id: name, species: 'Aesir'}) })
+      aesir.forEach(function(pair) { g.addVertex({ _id: pair[0]
+                                                 , species: 'Aesir'
+                                                 , gender: pair[1] == 'M' ? 'male' : 'female' }) })
       
       g.edges.should.have.lengthOf(0)
-      g.vertices.should.have.lengthOf(45)
+      g.vertices.should.have.lengthOf(42)
     })
     
     it('should add the Vanir', function() {
@@ -34,7 +41,7 @@ describe('Asgard', function() {
       vanir.forEach(function(name) { g.addVertex({_id: name, species: 'Vanir'}) })
       
       g.edges.should.have.lengthOf(0)
-      g.vertices.should.have.lengthOf(77)
+      g.vertices.should.have.lengthOf(74)
     })
     
     it('should add some edges', function() {
@@ -77,22 +84,44 @@ describe('Asgard', function() {
       })
       
       g.edges.should.have.lengthOf(27)
-      g.vertices.should.have.lengthOf(77)
+      g.vertices.should.have.lengthOf(74)
     })
   })
   
   describe('Queries from the chapter', function() {
     var thor
+    var getAesir = function(name) { return g.v(name).run()[0] }
     
     it("g.v('Thor') should be Thor", function() {
-      var out = g.v('Thor').out().in().run()
+      var out = g.v('Thor').run()
       thor = out[0]
       thor._id.should.equal('Thor')
       thor.species.should.equal('Aesir')
     })
     
-    it("g.v('Thor').out().in() should contain several copies of Thor", function() {
-      var out = g.v('Thor').out().in().run()
+    it("g.v('Thor', 'Odin') should be Thor and Odin", function() {
+      var out = g.v('Thor', 'Odin').run()
+      out.should.have.lengthOf(2)
+      out.should.contain(getAesir('Odin'))
+      out.should.contain(thor)
+    })
+    
+    it("g.v({species: 'Aesir'}) should be all Aesir", function() {
+      var out = g.v({species: 'Aesir'}).run()
+      out.should.have.lengthOf(42)
+      out.forEach(function(node) { node.should.have.property('species', 'Aesir') })
+    })
+    
+    it("g.v() should be all Aesir and Vanir", function() {
+      var out = g.v().run()
+      out.should.have.lengthOf(74)
+    })
+    
+    
+    it("g.v('Thor').in().out() should contain several copies of Thor, and his wives", function() {
+      var out = g.v('Thor').in().out().run()
+      out.should.contain(getAesir('Járnsaxa'))
+      out.should.contain(getAesir('Sif'))
       out.should.contain(thor)
       
       var out2 = g.v('Thor').out().in().unique().run()
@@ -102,24 +131,64 @@ describe('Asgard', function() {
       diff.should.be.above(0)
     })
     
-    it('should ...', function() {
-      // g.v('Thor').out('father').out('father').run().map(function(vertex) {return vertex.name})
-      // g.v('Thor').out('father').out('father').property('name').run()
+    it("g.v('Thor').in().in().out().out() should be the empty array, because we don't know Thor's grandchildren", function() {
+      var out = g.v('Thor').in().in().out().out().run()
+      out.should.deep.equal([])
+    })
+        
+    
+    it("g.v('Thor').out().in() should contain several copies of Thor, and his sibling", function() {
+      var out = g.v('Thor').out().in().run()
+      out.should.contain(getAesir('Baldr'))
+      out.should.contain(thor)
+      
+      var out2 = g.v('Thor').out().in().unique().run()
+      out2.should.contain(thor)
+      
+      var diff = out.length - out2.length
+      diff.should.be.above(0)
     })
     
-      // g.v('Thor').out().out().out().in().in().in()
-      // g.v('Thor').out().out().out().in().in().in().unique().take(10)
-      // g.v('Thor').out('father').in('father')
-      // g.v('Thor').out('father').out('father').run()
+      // g.v('Thor').out().in().unique().filter(function(asgardian) {return asgardian._id != 'Thor'}).run()
       
-      
-      // g.v('Thor').in().in().out().out().run()
-      // g.v('Thor').in().in().out().out().unique().run()
       // g.v('Thor').out().in().unique()
        // .filter(function(asgardian) { return asgardian.weight > asgardian.height })
        // .run()
-      // g.v('Thor').out().in().unique().filter({survives: true}).run()
-      // g.v('Thor').out().out().out().out().in().in().in().in().unique().take(12).run()
+    
+    
+    
+    it("g.v('Thor').out().in().unique().filter({survives: true}) should be the empty array, because we don't label survivors", function() {
+      var out = g.v('Thor').out().in().unique().filter({survives: true}).run()
+      out.should.deep.equal([])
+    })
+        
+    it("g.v('Thor').out().in().unique().filter({gender: 'male'}) should contain Thor and his sibling", function() {
+      var out = g.v('Thor').out().in().unique().filter({gender: 'male'}).run()
+      out.should.contain(getAesir('Baldr'))
+      out.should.contain(thor)
+    })
+        
+    it("g.v('Thor').out().out().out().in().in().in() should contain Thor and his sibling", function() {
+      var out = g.v('Thor').out().out().out().in().in().in().run()
+      out.should.contain(getAesir('Baldr'))
+      out.should.contain(thor)
+    })
+    
+    it("g.v('Thor').out().out().out().in().in().in().unique().take(10) should contain Thor and his sibling", function() {
+      var out = g.v('Thor').out().out().out().in().in().in().unique().take(10).run()
+      out.should.contain(getAesir('Baldr'))
+      out.should.contain(thor)
+    })
+    
+    it("g.v('Thor').out().out().out().out().in().in().in().in().unique().take(12) should contain Thor and his sibling", function() {
+      var out = g.v('Thor').out().out().out().out().in().in().in().in().unique().take(12).run()
+      out.should.contain(getAesir('Baldr'))
+      out.should.contain(thor)
+    })
+    
+    
+    
+    
       /*
          q = g.v('Auðumbla').in().in().in().property('name').take(1)
 
@@ -128,30 +197,13 @@ describe('Asgard', function() {
          q.run() // ["Vé"]
          q.run() // []
       */
+    
       // g.v('Thor').out().as('parent').out().as('grandparent').out().as('great-grandparent')
            // .merge('parent', 'grandparent', 'great-grandparent').run()
-      // g.v('Thor').out().in().unique().filter(function(asgardian) {return asgardian._id != 'Thor'}).run()
       // g.v('Thor').as('me').out().in().except('me').unique().run()
       // g.v('Thor').out().as('parent').out().in().except('parent').unique().run()
-      /*
-         g.v('Fjörgynn').in('daughter').as('me')                 // first gremlin's state.as is Frigg
-          .in()                                                  // first gremlin's vertex is now Baldr
-          .out().out()                                           // put copy of that gremlin on each grandparent
-          .filter({_id: 'Bestla'})                               // only keep the gremlin on grandparent Bestla
-          .back('me').unique().run()                             // jump the gremlin's vertex back to Frigg and exit
-      */
-      // g.v({_id:'Thor'}).run()` or `g.v({species: 'Aesir'}).run()
-      // g.v('Thor', 'Odin').run()
-      // g.v().run()
-      // g.v('Odin').in().run()
-      // g.v('Odin').in('son').run()
-      // g.v('Odin').in(['daughter', 'son']).run()
-      // g.v('Odin').in({position: 2, _label: daughter}).run()
-      // g.v('Thor').in().out().run()
-      // g.v('Thor').out().in()
-      // g.v('Thor').parents().children()
-      // g.v('Thor').children().parents()
-      
+
+    
       /*
          Dagoba.addAlias('parents', 'out', ['parent'])
          Dagoba.addAlias('children', 'in', ['parent'])
@@ -159,11 +211,41 @@ describe('Asgard', function() {
          Dagoba.addAlias('grandparents', [['out', 'parent'], ['out', 'parent']])
          Dagoba.addAlias('cousins', [['out', 'parent'], ['as', 'folks'], ['out', 'parent'], ['in', 'parent'], ['except', 'folks'], ['in', 'parents'], ['unique']])        
       */
+    
+      // g.v('Thor').parents().children()
+      // g.v('Thor').children().parents()
+    
       // g.v('Forseti').parents().as('parents').parents().children().except('parents').children().unique()
       // g.v('Forseti').cousins()
       
       // var q = g.v('Thor').children().children().take(2)
       // q.run()
+    
+      // g.v('Odin').in().run()
+      // g.v('Odin').in('son').run()
+      // g.v('Odin').in(['daughter', 'son']).run()
+      // g.v('Odin').in({position: 2, _label: daughter}).run()
+          
+      /*
+         g.v('Fjörgynn').in('daughter').as('me')                 // first gremlin's state.as is Frigg
+          .in()                                                  // first gremlin's vertex is now Baldr
+          .out().out()                                           // put copy of that gremlin on each grandparent
+          .filter({_id: 'Bestla'})                               // only keep the gremlin on grandparent Bestla
+          .back('me').unique().run()                             // jump the gremlin's vertex back to Frigg and exit
+      */
+    
+    
+    it('should ...', function() {
+      // same:
+      // g.v('Thor').out('father').out('father').run().map(function(vertex) {return vertex.name})
+      // g.v('Thor').out('father').out('father').property('name').run()
+
+      // g.v('Thor').out('father').in('father')
+      // g.v('Thor').out('father').out('father').run()
+      
+    })
+    
+      
       
       // adverbs:
       // g.v('Thor').out().as('parent').out().as('grandparent').out().as('great-grandparent').merge(['parent', 'grandparent', 'great-grandparent']).run()
